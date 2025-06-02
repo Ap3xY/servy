c3afa6600c9eaaffe248d6425ccc519a81fadd31
Alex <alexvdberg010109@gmail.com>
2025-06-02 23:07:15 +0200

    feat: implement route functionality to handle multiple requests

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,6 +2,7 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
@@ -13,26 +14,55 @@ defmodule Servy.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, response_body: ""}
+    %{method: method, path: path, status: nil, response_body: ""}
   end
 
+  def log(conversation_string), do: IO.inspect(conversation_string)
+
   def route(conversation_string) do
-    %{conversation_string | response_body: "Halo, CS2, COD"}
+    route(conversation_string, conversation_string.method, conversation_string.path)
+  end
+
+  def route(conversation_string, "GET", "/genres") do
+    %{conversation_string | status: 200, response_body: "Strategy, FPS, RPG"}
+  end
+
+  def route(conversation_string, "GET", "/games") do
+    %{
+      conversation_string
+      | status: 200,
+        response_body: "Total War Shogun 2, Call of Duty 3, Baldur's gate 3"
+    }
+  end
+
+  def route(conversation_string, "GET", "/games/" <> id) do
+    %{conversation_string | status: 200, response_body: "Game #{id}"}
+  end
+
+  def route(conversation_string, _method, path) do
+    %{conversation_string | status: 404, response_body: "No #{path} here!"}
   end
 
   def format_response(conversation_string) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conversation_string.status} #{status_reason(conversation_string.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conversation_string.response_body)}
 
     #{conversation_string.response_body}
     """
   end
+
+  defp status_reason(status_code) do
+    %{
+      200 => "OK",
+      404 => "Not Found"
+    }[status_code]
+  end
 end
 
 request = """
-GET /games HTTP/1.1
+GET /games/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
