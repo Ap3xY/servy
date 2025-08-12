0cb585d1dcc62723b03e59bac7e37288569b8a02
Alex van den Berg <alexvdberg010109@gmail.com>
2025-08-12 02:34:14 +0200

    feat(): implement recursion to loop through headers of post request

---
+++ lib/servy/conv.ex
@@ -0,0 +1,3 @@
+defmodule Servy.Conv do
+  defstruct method: "", path: "", params: %{}, headers: %{}, resp_body: "", status: nil
+end
--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,20 +3,20 @@ defmodule Servy.Handler do
   Handles HTTP requests.
   """
 
+  alias Servy.Conv
+
   @pages_path "pages/about.html"
+
+  import Servy.Parser, only: [parse: 1]
 
   @doc """
   Transforms the request into a response.
   """
   def handle(request) do
-    request |> parse() |> route() |> format_response()
-  end
-
-  def parse(request) do
-    [method, endpoint, _version] =
-      request |> String.split("\n") |> List.first() |> String.split(" ")
-
-    %{method: method, path: endpoint, status: nil, resp_body: ""}
+    request
+    |> parse()
+    |> route()
+    |> format_response()
   end
 
   def handle_file({:ok, content}, conv) do
@@ -31,19 +31,27 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "File error: #{reason}"}
   end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
+  def route(%Conv{method: "POST", path: "/frameworks"} = conv) do
+    %{
+      conv
+      | status: 201,
+        resp_body: "Create a #{conv.params["type"]} Framework named #{conv.params["name"]}!"
+    }
+  end
+
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
     File.read(@pages_path) |> handle_file(conv)
   end
 
-  def route(%{method: "GET", path: "/codinglanguages"} = conv) do
+  def route(%Conv{method: "GET", path: "/codinglanguages"} = conv) do
     %{conv | status: 200, resp_body: "C++, Python, Javascript"}
   end
 
-  def route(%{method: "GET", path: "/frameworks/" <> id} = conv) do
+  def route(%Conv{method: "GET", path: "/frameworks/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Framework #{id}"}
   end
 
-  def route(%{method: "GET", path: "/frameworks"} = conv) do
+  def route(%Conv{method: "GET", path: "/frameworks"} = conv) do
     %{conv | status: 200, resp_body: "NextJS, Springboot, Django"}
   end
 
@@ -51,7 +59,7 @@ defmodule Servy.Handler do
     %{conv | status: 404, resp_body: "No #{path}, here!"}
   end
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
     """
     HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
@@ -70,11 +78,14 @@ defmodule Servy.Handler do
 end
 
 request = """
-GET /about HTTP/1.1
+POST /frameworks HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
 
+name=NextJS&type=React
 """
 
 response = Servy.Handler.handle(request)
