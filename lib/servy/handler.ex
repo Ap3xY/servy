3ffaa2f5219687eee45a290f845f2f8ffe27bd70
Alex van den Berg <alexvdberg010109@gmail.com>
2026-02-13 14:03:22 +0200

    refactor: remove duplicated serving page logic

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -45,15 +45,20 @@ defmodule Servy.Handler do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
+  def route(%{ method: "GET", path: "/bears/new"} = conv) do
+    serve_page(conv, "form.html")
+  end
+
   def route(%{ method: "GET", path: "/bears/" <> id} = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}"}
   end
 
   def route(%{ method: "GET", path: "/about"} = conv) do
-    Path.expand("../../pages", __DIR__)
-      |> Path.join("about.html")
-      |> File.read()
-      |> handle_file(conv)
+    serve_page(conv, "about.html")
+  end
+
+  def route(%{ path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def handle_file({:ok, content}, conv) do
@@ -66,10 +71,6 @@ defmodule Servy.Handler do
 
   def handle_file({:error, reason}, conv) do
     %{ conv | status: 500, resp_body: "File error: #{reason}"}
-  end
-
-  def route(%{ path: path} = conv) do
-    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def track(%{status: 404, path: path} = conv) do
@@ -98,6 +99,14 @@ defmodule Servy.Handler do
       404 => "Not Found",
       500 => "Internal Server Error"
     }[code]
+  end
+
+  defp serve_page(conv, file_name) do
+    "../../pages"
+    |> Path.expand(__DIR__)
+    |> Path.join(file_name)
+    |> File.read()
+    |> handle_file(conv)
   end
 end
 
@@ -151,6 +160,18 @@ IO.puts(response)
 
 request = """
 GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bears/new HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
