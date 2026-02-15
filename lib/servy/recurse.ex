a5329c0265aa710f0790aae7294cd82e3dd8cde0
Alex van den Berg <alexvdberg010109@gmail.com>
2026-02-15 22:55:30 +0200

    Exercise: Triple All The Numbers

--- lib/servy/recurse.ex
+++ lib/servy/recurse.ex
@@ -4,6 +4,14 @@ defmodule Recurse do
   end
 
   def sum([], total), do: IO.puts("#{total}")
+
+  def triple([head | tail]) do
+    [head * 3 | triple(tail)]
+  end
+
+  def triple([]), do: []
 end
 
 Recurse.sum([1, 2, 3, 3], 0)
+
+IO.inspect(Recurse.triple([1, 2, 3, 4, 5]))
